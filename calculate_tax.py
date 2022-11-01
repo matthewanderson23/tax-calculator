e3d926d1c933c89cea8b593d41ffdfb7e996f42e
matthewanderson23 <matthewdanderson23@gmail.com>
2022-11-01 15:46:04 -0700

    Show subtotal/total, add discount option

--- calculate_tax.py
+++ calculate_tax.py
@@ -10,10 +10,11 @@ def _split_by_comma_and_remove_whitespace(string: str):
 
 
 def ask_for_attendees():
-    prompt = f"Enter the names of attendees at the event, separated by commas: "
+    prompt = "Enter the names of attendees at the event, separated by commas: "
     attendees_str = input(prompt)
     attendees = _split_by_comma_and_remove_whitespace(attendees_str)
     return attendees
+
 
 def prompt_for_subtotal(attendee: Attendee):
     prompt = f"Enter the amount of each item for {attendee.name}, separated by commas: "
@@ -22,13 +23,22 @@ def prompt_for_subtotal(attendee: Attendee):
     subtotal = sum([float(eval(i)) for i in amounts])
     return subtotal
 
+
 def prompt_for_tax():
     tax = float(input("What was the amount in taxes paid? "))
     return tax
 
+
 def prompt_for_tips():
     tips = float(input("What was the amount in tips/gratuities paid? "))
     return tips
+
+
+def prompt_for_discounts():
+    prompt = "What was the amount in discounts applied? (Will be applied to whole bill) "
+    amt_discount = float(input(prompt))
+    return amt_discount
+
 
 def main():
     people = ask_for_attendees()
@@ -41,8 +51,9 @@ def main():
 
     tax = prompt_for_tax()
     tips = prompt_for_tips()
+    amt_discount = prompt_for_discounts()
 
-    tc = TaxCalculator(tax + tips, attendees)
+    tc = TaxCalculator(tax, tips - amt_discount, attendees)
     tc.calculate()
     tc.display_totals()
 
--- tax_calculator.py
+++ tax_calculator.py
@@ -8,11 +8,12 @@ class Attendee:
         self.name: str = name
         self.subtotal: Optional[float] = None
         self.tax_portion: Optional[float] = None
+        self.tip_portion: Optional[float] = None
         self._total: Optional[float] = None
 
     @property
     def total(self):
-        self._total = self.subtotal + self.tax_portion
+        self._total = self.subtotal + self.tax_portion + self.tip_portion
         return self._total
 
     def calculate_subtotal(self, bill_amounts: str):
@@ -24,11 +25,12 @@ class Attendee:
 
 
 class TaxCalculator:
-    def __init__(self, tax_amount: float, attendees: List[Attendee]):
-        self.tax: float = tax_amount
+    def __init__(self, tax_amount: float, tip_amount: float, attendees: List[Attendee]):
+        self.tax= tax_amount
+        self.tip = tip_amount
         self.attendees: List[Attendee] = attendees
 
-        self._event_total: Optional[float] = None
+        self._event_total: float = 0.0
         self._group_subtotal: float = 0.0
 
     def calculate(self):
@@ -36,16 +38,31 @@ class TaxCalculator:
             self._group_subtotal += attendee.subtotal
         for attendee in self.attendees:
             attendee.tax_portion = self._calculate_tax_per(attendee)
+            attendee.tip_portion = self._calculate_tip_per(attendee)
 
     def _calculate_tax_per(self, attendee: Attendee):
         return self.tax * attendee.subtotal / self._group_subtotal
 
+    def _calculate_tip_per(self, attendee: Attendee):
+        return self.tip * attendee.subtotal / self._group_subtotal
+
     @property
     def event_total(self):
-        raise NotImplementedError
+        total = 0
+        for attendee in self.attendees:
+            total = total + attendee.total
+        self._event_total = total
+        return self._event_total
 
     def display_totals(self):
         print('\n')
+        print(f"Subtotal: {round(self._group_subtotal, 2)}")
         for attendee in self.attendees:
-            print(attendee.name + ' | TAX: ' + str(round(attendee.tax_portion, 2)) + ' | TOTAL: ' + str(round(attendee.total, 2)))
+            print(
+                attendee.name 
+                + ' | TAX: ' + str(round(attendee.tax_portion, 2)) 
+                + ' | TIP: ' + str(round(attendee.tip_portion, 2)) 
+                + ' | TOTAL: ' + str(round(attendee.total, 2))
+            )
+        print(f"Total bill: {round(self.event_total, 2)}")
 
